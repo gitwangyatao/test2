317007b8cb9514b6ac5b6b29947a7f3a1a778a4d
wyt <18606246651@163.com>
2019-12-26 16:10:21 +0800

    cls

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+public Class HelloWorld(){
+	public Static void main(String[] args){
+		System.out.println("Hello World");
+		System.out.println("1");
+		System.out.println("Hello World");
+		System.out.println("2");
+	}
+}
